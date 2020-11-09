486f0ffc605de4f6f9ec057d173543ea0d1a8b6c
Dave Mateer <davemateer@gmail.com>
2020-11-09 07:42:21 +0000

    auto

--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@passwordpostgrestest906.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@passwordpostgrestest948.westeurope.cloudapp.azure.com
 
