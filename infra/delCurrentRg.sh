159bfbdac4ba24de30eac97d9c9c336b276db715
Dave Mateer <davemateer@gmail.com>
2020-11-26 10:36:31 +0000

    auto

--- infra/delCurrentRg.sh
+++ infra/delCurrentRg.sh
@@ -1,2 +1,2 @@
-az group delete --name PasswordPostgresTEST662 --no-wait --yes 
+az group delete --name PasswordPostgresTEST337 --no-wait --yes 
 
--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@passwordpostgrestest662.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@passwordpostgrestest337.westeurope.cloudapp.azure.com
 
