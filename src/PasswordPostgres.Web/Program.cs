066a3f924693ead2bbbd390d60eaae717a5bffe6
Dave Mateer <davemateer@gmail.com>
2021-03-11 10:26:07 +0000

    auto

--- src/PasswordPostgres.Web/Program.cs
+++ src/PasswordPostgres.Web/Program.cs
@@ -16,7 +16,7 @@ namespace PasswordPostgres.Web
                 //.Filter.ByExcluding("RequestPath like '/health%'")
                 //.Filter.ByExcluding("SourceContext <> 'Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware'")
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Debug)
+                //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Verbose)
                 // if only do warning, then will get duplicate error messages when an exception is thrown, then again when re-executed
                 // we do get 2 error message per single error, but only 1 stack trace
                 //.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Fatal)
@@ -27,8 +27,8 @@ namespace PasswordPostgres.Web
                 .WriteTo.File(@"logs/info.txt", restrictedToMinimumLevel: LogEventLevel.Information, rollingInterval: RollingInterval.Day)
                 //.WriteTo.Seq(Environment.GetEnvironmentVariable("SEQ_URL") ?? "http://localhost:5341")
 
-                .Filter.ByExcluding("SourceContext = 'Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware'")
-                .WriteTo.File(@"logs/warning2.txt", restrictedToMinimumLevel: LogEventLevel.Warning, rollingInterval: RollingInterval.Day)
+                //.Filter.ByExcluding("SourceContext = 'Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware'")
+                //.WriteTo.File(@"logs/warning2.txt", restrictedToMinimumLevel: LogEventLevel.Warning, rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
             try
